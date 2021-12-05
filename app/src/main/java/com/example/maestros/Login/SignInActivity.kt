38b4f0b412a1fdca1764dd1d27d0bad39436fac5
alanurdgz <nalan3947@gmail.com>
2021-12-04 18:37:21 -0600

    Primer commit: Vista SignIn y dependencias de firebase

---
+++ app/src/main/java/com/example/maestros/Login/SignInActivity.kt
@@ -0,0 +1,13 @@
+package com.example.maestros.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.maestros.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
