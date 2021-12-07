856f366968b7fb67a52f1b4eb691fe6fec5c1bb5
alanurdgz <nalan3947@gmail.com>
2021-12-06 19:07:02 -0600

    Octavo commit: vista main materias

--- app/src/main/java/com/example/maestros/MainActivity.kt
+++ app/src/main/java/com/example/maestros/MainActivity.kt
@@ -3,11 +3,27 @@ package com.example.maestros
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.maestros.Login.AccountActivity
+import com.example.maestros.Materias.Materias
 import com.example.maestros.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.materias_content.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,5 +43,107 @@ class MainActivity : AppCompatActivity() {
             intent = Intent(this, AccountActivity::class.java)
             startActivity(intent)
         }
+
+        binding.newFloatingActionButton.setOnClickListener {
+            intent = Intent(this, AddActivity::class.java)
+            startActivity(intent)
+        }
+
+        listMaterias.clear()
+        setupRecyclerView(materiasRecyclerView)
+    }
+
+    private fun setupRecyclerView(recyclerView: RecyclerView) {
+
+        messagesListener = object : ValueEventListener {
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                listMaterias.clear()
+                dataSnapshot.children.forEach { child ->
+                    val materias: Materias? =
+                        Materias(child.child("name").getValue<String>(),
+                            child.child("date").getValue<String>(),
+                            child.child("hora").getValue<String>(),
+                            child.child("description").getValue<String>(),
+                            child.child("url").getValue<String>(),
+                            child.key)
+                    materias?.let { listMaterias.add(it) }
+                }
+                recyclerView.adapter = MateriasViewAdapter(listMaterias)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("TAG", "messages:onCancelled: ${error.message}")
+            }
+        }
+        myRef.addValueEventListener(messagesListener)
+
+        deleteSwipe(recyclerView)
+    }
+
+    class MateriasViewAdapter(private val values: List<Materias>) :
+        RecyclerView.Adapter<MateriasViewAdapter.ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.materias_content, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val materias = values[position]
+            holder.mNameTextView.text = materias.name
+            holder.mDateTextView.text = materias.date
+            holder.mHoraTextView.text = materias.hora
+            holder.mPosterImgeView?.let {
+                Glide.with(holder.itemView.context)
+                    .load(materias.url)
+                    .into(it)
+            }
+
+            holder.itemView.setOnClickListener { v ->
+                val intent = Intent(v.context, MateriasDetailActivity::class.java).apply {
+                    putExtra("key", materias.key)
+                }
+                v.context.startActivity(intent)
+            }
+
+
+            //Editar
+            /*holder.itemView.setOnLongClickListener{ v ->
+                val intent = Intent(v.context, EditActivity::class.java).apply {
+                    putExtra("key", materias.key)
+                }
+                v.context.startActivity(intent)
+                true
+            }*/
+
+        }
+
+        override fun getItemCount() = values.size
+
+        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            val mNameTextView: TextView = view.nameTextView
+            val mDateTextView: TextView = view.dateTextView
+            val mHoraTextView: TextView = view.horaTextView
+            val mPosterImgeView: ImageView? = view.posterImgeView
+        }
+    }
+
+    private fun deleteSwipe(recyclerView: RecyclerView){
+        val touchHelperCallback: ItemTouchHelper.SimpleCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                listMaterias.get(viewHolder.adapterPosition).key?.let { myRef.child(it).setValue(null) }
+                listMaterias.removeAt(viewHolder.adapterPosition)
+                recyclerView.adapter?.notifyItemRemoved(viewHolder.adapterPosition)
+                recyclerView.adapter?.notifyDataSetChanged()
+            }
+        }
+        val itemTouchHelper = ItemTouchHelper(touchHelperCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
     }
 }
